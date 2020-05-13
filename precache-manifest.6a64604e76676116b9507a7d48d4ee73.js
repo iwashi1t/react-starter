24108c5d073b1e8b9af424368df604df4786c31b
iwashita <iwashita@supersoftware.co.jp>
2020-05-13 10:54:39 +0900

    Updates

---
+++ precache-manifest.6a64604e76676116b9507a7d48d4ee73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba59ff5a4edcb93256f83c6c22c39f80",
+    "url": "/react-starter/index.html"
+  },
+  {
+    "revision": "49d4dbb7db61f17b1140",
+    "url": "/react-starter/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a06c62ecdb50d4eceb0e",
+    "url": "/react-starter/static/js/2.f8545b99.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/react-starter/static/js/2.f8545b99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49d4dbb7db61f17b1140",
+    "url": "/react-starter/static/js/main.89b0561f.chunk.js"
+  },
+  {
+    "revision": "e6874049c7392457c259",
+    "url": "/react-starter/static/js/runtime-main.fcb3e255.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-starter/static/media/logo.5d5d9eef.svg"
+  }
+]);
