6e64cb4d88a4fe4993ff922494e6e4134ab3fe92
iwashita <iwashita@supersoftware.co.jp>
2020-05-13 10:50:39 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Pocket from '@natade-coco/pocket-sdk';
+import EMV from '@natade-coco/emv-code';
+
 function App() {
   return (
     <div className="App">
@@ -18,6 +21,49 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={() => {
+          Pocket.getCCInfo()
+            .then(response => alert(JSON.stringify(response)))
+            .catch(error => alert(error));
+        }}>
+          Get CCInfo
+        </button>
+        <button onClick={() => {
+          Pocket.getProfile(['nickname', 'name', 'tel', 'image'])
+            .then(response => alert(JSON.stringify(response)))
+            .catch(error => alert(error));
+        }}>
+          Get Profile
+        </button>
+        <button onClick={() => {
+          Pocket.getEthereumAddress()
+            .then(response => alert(JSON.stringify(response)))
+            .catch(error => alert(error));
+        }}>
+          Get EthereumAddress
+        </button>
+        <button onClick={() => {
+          const target = 'did:ethr:0x76113d46cd96164cd273f0f540e869c25ff7d240';
+          const path = `/orders/order-1111`;
+          const allows = ['did:ethr:0x076a26dd8b99750d0123a63c28ea72a336e90114'];
+          Pocket.connectToPayoffApp(target, path, allows)
+            .then(response => alert(JSON.stringify(response)))
+            .catch(error => alert(error));
+        }}>
+          Connect to PayoffApp
+        </button>
+        <button onClick={() => {
+          Pocket.requestSignJWT()
+            .then(response => alert(JSON.stringify(response)))
+            .catch(error => alert(error));
+        }}>
+          Request SignJWT
+        </button>
+        <button onClick={() => {
+          EMV.getJPQR();
+        }}>
+          Get JPQR
+        </button>
       </header>
     </div>
   );
